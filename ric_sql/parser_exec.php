68c4cf88dd18b7208efbfcd28ea3477ab579fc0e
Farinotti Daniele <daniele.farinotti@tin.it>
2020-05-29 09:39:59 +0200

    Add files via upload

--- ric_sql/parser_exec.php
+++ ric_sql/parser_exec.php
@@ -1,5 +1,5 @@
 <?
-namespace Cheope_ppp_ns\ric_sql;
+namespace Cheope_ppg_ns\ric_sql;
 
 require_once("ric_sql_2_def.php");
 require_once("../src/Parser.php");
@@ -9,10 +9,10 @@ if(isset($argv[1]))
 else
  $inputFileName = "example1.txt";
 
-$lex = new \Cheope_ppp_ns\src\Lexer_3($inputFileName);
+$lex = new \Cheope_ppg_ns\src\Lexer_3($inputFileName);
 $lex->setRules($ricSql2DefRules);
 
-$parser = new \Cheope_ppp_ns\src\Parser($lex);
+$parser = new \Cheope_ppg_ns\src\Parser($lex);
 $parser->setGrammarRulesContainer($ricSql2DefGrRules);
 $parser->setLogEnabled(true);
 //$parser->setEnableLogOnFile(true);
